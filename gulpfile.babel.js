141118646ee98098a6dd1bc554b3d2815a64e59a
Bhaskar Karambelkar <bhaskarvk@gmail.com>
2017-04-06 12:04:14 -0400

    So close

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,56 +1,58 @@
-'use babel';
+'use babel'
 
 import gulp from 'gulp'
 import runSequence from 'run-sequence'
 import stylus from 'gulp-stylus'
 import cleanCSS from 'gulp-clean-css'
 import sourcemaps from 'gulp-sourcemaps'
-import rename  from 'gulp-rename'
+import rename from 'gulp-rename'
 import clean from 'gulp-clean'
 import autoprefixer from 'gulp-autoprefixer'
 
 let cssSrcDir = 'src/css'
 let cssDestDir = 'static/css'
 
-gulp.task('clean-css', function() {
-  return gulp.src(cssDestDir)
-    .pipe(clean())
+gulp.task('clean-css', function () {
+    return gulp.src(cssDestDir)
+    	.pipe(clean())
 })
 
-gulp.task('stylus-to-css', function() {
-  return gulp.src([cssSrcDir+'/*.styl'])
-    .pipe(stylus())
-    .pipe(gulp.dest(cssDestDir))
+gulp.task('stylus-to-css', function () {
+    return gulp.src([cssSrcDir + '/*.styl'])
+    	.pipe(stylus())
+    	.pipe(gulp.dest(cssDestDir))
 })
 
 gulp.task('minify-css', function () {
-  return gulp.src([cssDestDir+'/*.css','!'+cssDestDir+'/*.min.css'])
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
-      browsers: ['last 4 versions'],
-      cascade: false
-    }))
-    .pipe(cleanCSS(
-      {debug: true, inline: 'none'},
-      function(details) {
-        console.log(details.name + ': ' + details.stats.originalSize
-           + " => " + details.stats.minifiedSize)
-      }
-    ))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(cssDestDir));
-});
-
-gulp.task('build-css', (callback) => {
-  runSequence('clean-css','stylus-to-css', 'minify-css', callback)
+    return gulp.src([cssDestDir + '/*.css', '!' + cssDestDir + '/*.min.css'])
+    	.pipe(sourcemaps.init())
+    	.pipe(autoprefixer({
+    		browsers: ['last 4 versions'],
+    		cascade: false
+    	}))
+    	.pipe(cleanCSS(
+    		{ debug: true, inline: 'none' },
+    		function (details) {
+    			console.log(details.name + ': ' + details.stats.originalSize +
+    				' => ' + details.stats.minifiedSize)
+    		}
+    	))
+    	.pipe(rename({ suffix: '.min' }))
+    	.pipe(sourcemaps.write('./'))
+    	.pipe(gulp.dest(cssDestDir))
 })
 
-gulp.task('build', (callback) => {
-  runSequence('build-css', callback)
+gulp.task('build-css', (callback) => {
+    runSequence('stylus-to-css', 'minify-css', callback)
+})
+
+gulp.task('clean', ['clean-css'])
+
+gulp.task('build', ['clean'], (callback) => {
+    runSequence('build-css', callback)
 })
 
 // Watch asset folder for changes
-gulp.task("watch", ["build-css"], function () {
-  gulp.watch(cssSrcDir+'/**/*.styl', ["build-css"])
+gulp.task('watch', ['build'], function () {
+    gulp.watch(cssSrcDir + '/**/*.styl', ['build-css'])
 })
